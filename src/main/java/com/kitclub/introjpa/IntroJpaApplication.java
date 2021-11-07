3eb3795ecc402e31dd5371047a0b57611323ec06
samukma <nguyensam576@gmail.com>
2021-11-07 09:25:10 +0700

    create project

---
+++ src/main/java/com/kitclub/introjpa/IntroJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kitclub.introjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kitclub/introjpa/IntroJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitclub.introjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
